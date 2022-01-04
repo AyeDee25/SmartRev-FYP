3b75e4f534e54b6fbfb6b99a2a2c28cb18c7de52
AyeDee25 <mrusyaidi27@yahoo.com>
2022-01-04 15:43:41 +0800

    Update Landing.js

--- SmartRev/client/components/auth/Landing.js
+++ SmartRev/client/components/auth/Landing.js
@@ -5,7 +5,6 @@ export default function Landing({ navigation }) {
     return (
         <View style = {{ flex: 1, justifyContent: 'center'}}>
             <Button title="Register" onPress = {() => navigation.navigate("Register")}/>
-
             <Button title="Login" onPress = {() => navigation.navigate("Login")}/>
 
         </View>
